e7342924ffe22ecd7b962e7528991ab7b3a57788
Ryockans <pavekon99@yandex.com>
2022-11-23 19:22:09 +0300

    some fixes

--- src/components/similar-movies/similar-movies.tsx
+++ src/components/similar-movies/similar-movies.tsx
@@ -2,13 +2,18 @@ import styles from './similar-movies.module.css';
 import { useSelector } from 'react-redux';
 import { movieListSelector } from '../../store/slices/movie-list/selectors';
 import MovieList from '../movie-list';
+import { movieSelector } from '../../store/slices/movie/selectors';
 
 export const SimilarMovies = () => {
+  const { movieInfo: currentMovie } = useSelector(movieSelector);
   const { movies } = useSelector(movieListSelector);
+
+  const similarMovies = movies.filter((movie) => movie.id !== currentMovie.id);
+
   return (
     <section className={styles.similar}>
       <h2 className={styles.title}>More like this</h2>
-      <MovieList movieList={movies} />
+      <MovieList movieList={similarMovies} />
     </section>
   );
 };
